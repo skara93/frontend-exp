f55fb9b4ea989674a6cba021fe166a188ee55657
skara93 <skara93@mail.ru>
2020-02-20 21:59:38 +0300

    landing

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const fs = require('fs');
 const server = http.createServer(function(request, response) {
     console.log(request.method, request.url);
     if(request.url == '/style.css') {
-        const css = fs.readFile('style.css', 'utf8');
+        const css = fs.readFileSync('style.css', 'utf8');
         response.end(css);
     } else {
-        const html = fs.readFile('index.html', 'utf8');
+        const html = fs.readFileSync('index.html', 'utf8');
         response.end(html);
     }
-
+});
     server.listen(process.env.PORT || 3000);
     console.log('Server started!');
-});
